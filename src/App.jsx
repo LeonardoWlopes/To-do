46db95c43a22df268484345a35c75432442aeeea
Leonardo William Lopes <leonardolopessocial@gmail.com>
2021-10-10 23:14:00 -0300

    Out10 - 23:15

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+import './App.css'
+import AddTask from './components/AddTask'
+
+import Tasks from './components/Tasks'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Aprender React',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Sei la',
+      completed: true
+    }
+  ])
+  
+  const handleTaskAddition = (taskTitle) => {
+      const newTasks = [
+      ... tasks,
+      {
+        id: Math.random(10),
+        title: taskTitle,
+        completed: false
+      }
+    ]
+    setTasks(newTasks)
+  }
+
+  return(
+    <>
+    <div className='container'>
+      <AddTask handleTaskAddition={handleTaskAddition}/>
+      <Tasks tasks={tasks}/>
+    </div>
+    </>
+  )
+}
+export default App
