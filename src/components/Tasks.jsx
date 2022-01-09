baf0465aa38f638b00d9efb8f37da44ddde75330
Leonardo William Lopes <62454440+LeonardoWlopes@users.noreply.github.com>
2022-01-09 00:25:24 -0300

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,14 +5,17 @@ import Task from "./Task";
 const Tasks = ({ tasks, handleTaskClick, handleDeleteTask }) => {
   return (
     <>
-      {tasks?.map((task) => (
-        <Task
-          task={task}
-          handleTaskClick={handleTaskClick}
-          handleDeleteTask={handleDeleteTask}
-          key={task.id}
-        />
-      ))}
+      {tasks?.map(
+        (task) =>
+          !!task.title && (
+            <Task
+              task={task}
+              handleTaskClick={handleTaskClick}
+              handleDeleteTask={handleDeleteTask}
+              key={task.id}
+            />
+          )
+      )}
     </>
   );
 };
